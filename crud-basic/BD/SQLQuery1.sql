c9b8621f5bf5ee9092016963f555167e468999b5
Ana Isabel Cuéllar Maestro <100219176+anaicm@users.noreply.github.com>
2023-11-03 20:07:45 +0100

    Base de datos libro y usuario

---
+++ crud-basic/BD/SQLQuery1.sql
@@ -0,0 +1,22 @@
+USE [biblioteca]
+GO
+
+/****** Object:  Table [dbo].[Libro]    Script Date: 25/10/2023 21:31:29 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Libro](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[titulo] [nvarchar](1000) NOT NULL,
+	[Autor] [nvarchar](1000) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
